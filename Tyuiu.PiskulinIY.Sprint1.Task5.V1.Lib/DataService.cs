e44aa9f4da86c06f8a73c50091436cd6b2d9f7ca
TeleKxd <piskulin.i@mail.ru>
2024-10-15 21:41:13 +0300

    Task 5

--- Tyuiu.PiskulinIY.Sprint1.Task5.V1.Lib/DataService.cs
+++ Tyuiu.PiskulinIY.Sprint1.Task5.V1.Lib/DataService.cs
@@ -6,10 +6,7 @@ namespace Tyuiu.PiskulinIY.Sprint1.Task5.V1.Lib
         public int DistanceBetweenDots(double x1, double y1, double x2, double y2)
         {
 
-            double res = Math.Sqrt(((x2 - x1) * (x2 - x1)) + ((y2 - y1) * (y2 - y1)));
-
-
-            return (int)res;
+            return (int)Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2));
 
 
 
--- Tyuiu.PiskulinIY.Sprint1.Task5.V1.Test/DataServiceTest.cs
+++ Tyuiu.PiskulinIY.Sprint1.Task5.V1.Test/DataServiceTest.cs
@@ -9,19 +9,12 @@ namespace Tyuiu.PiskulinIY.Sprint1.Task5.V1.Test
         {
 
             DataService ds = new DataService();
-
-            double x1 = 5;
-            double x2 = 2;
-            double y1 = 5;
+            double x1 = 1;
+            double y1 = 1;
+            double x2 = 1;
             double y2 = 1;
-
-            double res = ds.DistanceBetweenDots(x1, x2, y1, y2);
-
-            int result = Convert.ToInt32(res);
-
-            int wait = 5;
-
-            Assert.AreEqual(wait, result);
+            var res = ds.DistanceBetweenDots(x1, y1, x2, y2);
+            Assert.AreEqual(0, res);
 
         }
     }
